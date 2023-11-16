c60cfa31c1711e604c571d7964bf02bd3a64ee1b
marcorexo <dyfroedd@gmail.com>
2023-11-16 11:13:39 +0000

    encrypt function

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ alphabet = [
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+#e.g.
+#plain_text = "hello"
+#shift = 5
+#cipher_text = "mjqqt"
+#print output: "The encoded text is mjqqt"
+
+
+def encrypt(text, shift):
+    encrypted_text = ""
+
+    for letter in text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift
+            encrypted_text += alphabet[new_position]
+    return encrypted_text
+
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-
-def encrypt(text, shift):
-    pass
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+print(encrypt(text=text, shift=shift))
+
+##HINT: How do you get the index of an item in a list:
+#https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
